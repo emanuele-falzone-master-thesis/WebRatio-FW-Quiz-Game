01178d4365c25215c76fe38e6bf6c6fea325bd13
Emanuele Falzone <emafalzo@gmail.com>
2018-10-11 18:22:16 +0200

    R4

--- FunergyJournal/.Temp~/components/src/_wr_root.js
+++ FunergyJournal/.Temp~/components/src/_wr_root.js
@@ -13,27 +13,30 @@
 	import * as mod3 from "wrm\/comp\/ListService";
 	wrm.defineModule("wrm\/comp\/ListService", mod3)
 
-	import * as mod4 from "wrm\/comp\/SelectorService";
-	wrm.defineModule("wrm\/comp\/SelectorService", mod4)
+	import * as mod4 from "wrm\/comp\/FormService";
+	wrm.defineModule("wrm\/comp\/FormService", mod4)
 
-	import * as mod5 from "wrm\/comp\/SwitchService";
-	wrm.defineModule("wrm\/comp\/SwitchService", mod5)
+	import * as mod5 from "wrm\/comp\/SelectorService";
+	wrm.defineModule("wrm\/comp\/SelectorService", mod5)
 
-	import * as mod6 from "wrm\/comp\/CreateService";
-	wrm.defineModule("wrm\/comp\/CreateService", mod6)
+	import * as mod6 from "wrm\/comp\/SwitchService";
+	wrm.defineModule("wrm\/comp\/SwitchService", mod6)
 
-	import * as mod7 from "wrm\/comp\/DeleteService";
-	wrm.defineModule("wrm\/comp\/DeleteService", mod7)
+	import * as mod7 from "wrm\/comp\/CreateService";
+	wrm.defineModule("wrm\/comp\/CreateService", mod7)
 
-	import * as mod8 from "wrm\/comp\/UpdateService";
-	wrm.defineModule("wrm\/comp\/UpdateService", mod8)
+	import * as mod8 from "wrm\/comp\/DeleteService";
+	wrm.defineModule("wrm\/comp\/DeleteService", mod8)
 
-	import * as mod9 from "testLocaleComponent\/ChangeLocaleService";
-	wrm.defineModule("testLocaleComponent\/ChangeLocaleService", mod9)
+	import * as mod9 from "wrm\/comp\/UpdateService";
+	wrm.defineModule("wrm\/comp\/UpdateService", mod9)
 
-	import * as mod10 from "wrm\/comp\/BarcodeOperationService";
-	wrm.defineModule("wrm\/comp\/BarcodeOperationService", mod10)
+	import * as mod10 from "testLocaleComponent\/ChangeLocaleService";
+	wrm.defineModule("testLocaleComponent\/ChangeLocaleService", mod10)
 
-	import * as mod11 from "wrm\/comp\/RestfulRequestResponseService";
-	wrm.defineModule("wrm\/comp\/RestfulRequestResponseService", mod11)
+	import * as mod11 from "wrm\/comp\/BarcodeOperationService";
+	wrm.defineModule("wrm\/comp\/BarcodeOperationService", mod11)
+
+	import * as mod12 from "wrm\/comp\/RestfulRequestResponseService";
+	wrm.defineModule("wrm\/comp\/RestfulRequestResponseService", mod12)
 
